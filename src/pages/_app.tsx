1f81d71822ca55b293abc62924d028f693e45ec2
MoisesSiqueira <151725115+MoisesSiqueira@users.noreply.github.com>
2024-03-18 19:10:36 -0300

    Mensagem do commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { AppProps } from "next/app";
+import "@fontsource/inter"; // Importa a fonte Inter do @fontsource
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove o estilo de fonte padrão do Next.js para evitar FOUC (Flash of Unstyled Content)
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
